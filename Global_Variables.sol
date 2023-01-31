d72055612a4b4ddfb074b570633482b7cabbddb0
Mohsin Hafeez <47477844+mohman23@users.noreply.github.com>
2023-01-31 13:24:30 +0530

    Update Global_Variables.sol
    //this: refers to the contract itself.
    //msg: this refers to how the contract execution begun, who forced the contract to start executing.
    //block: this refers to the information about the block that the transaction being sent to this smart contract is included in.
    //tx: this stands for transaction and gives us information about the transaction.
    
    //msg.value: this is going to reference how much ethereum was sent to the function or to the smart contract from the transaction that came in.
    
    //https://www.tutorialspoint.com/solidity/solidity_variables.htm
    
    //https://www.evernote.com/shard/s417/sh/b7b255fb-4fca-7799-208e-d5272643115e/fef12b6d36fa6319cc2ee68b3464ce38

--- Global_Variables.sol
+++ Global_Variables.sol
@@ -24,3 +24,14 @@ contract GetBalance {
     }
 
 }
+
+//this: refers to the contract itself.
+//msg: this refers to how the contract execution begun, who forced the contract to start executing.
+//block: this refers to the information about the block that the transaction being sent to this smart contract is included in.
+//tx: this stands for transaction and gives us information about the transaction.
+
+//msg.value: this is going to reference how much ethereum was sent to the function or to the smart contract from the transaction that came in.
+
+//https://www.tutorialspoint.com/solidity/solidity_variables.htm
+
+//https://www.evernote.com/shard/s417/sh/b7b255fb-4fca-7799-208e-d5272643115e/fef12b6d36fa6319cc2ee68b3464ce38
